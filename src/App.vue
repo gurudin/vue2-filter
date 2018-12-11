e4234a01d541426c2a6b0c188cb767f38a7ac4e1
gaoxiang <4008353@qq.com>
2018-12-11 15:40:37 +0800

    fixed currency

--- src/App.vue
+++ src/App.vue
@@ -8,6 +8,10 @@
     <p><kbd v-html="toUcfirst"></kbd> => <code>{{'gurudin' | toUcfirst}}</code></p>
     <p><kbd v-html="toLcfirst"></kbd> => <code>{{'GURUDIN' | toLcfirst}}</code></p>
     <p><kbd v-html="truncate"></kbd> => <code>{{'Praeterea iter est quasdam res quas ex communi.' | truncate}}</code></p>
+    <p><kbd v-html="truncate1"></kbd> => <code>{{'Praeterea iter est quasdam res quas ex communi.' | truncate(5, '###')}}</code></p>
+    <p><kbd v-html="currency"></kbd> => <code>{{123456789 | currency}}</code></p>
+    <p><kbd v-html="currency1"></kbd> => <code>{{'123456789' | currency('￥', 0)}}</code></p>
+    <p><kbd v-html="currency2"></kbd> => <code>{{'123456789' | currency('usd', 2, 'last')}}</code></p>
   </div>
 </template>
 
@@ -23,6 +27,10 @@ export default {
       toUcfirst: "{{'gurudin' | toUcfirst}}",
       toLcfirst: "{{'GURUDIN' | toLcfirst}}",
       truncate: "{{'Praeterea iter est quasdam res quas ex communi.' | truncate}}",
+      truncate1: "{{'Praeterea iter est quasdam res quas ex communi.' | truncate(5, '###')}}",
+      currency: "{{123456789 | currency}}",
+      currency1: "{{'123456789' | currency('￥', 0)}}",
+      currency2: "{{'123456789' | currency('usd', 2, 'last')}}",
     };
   }
 }
--- src/strings/convert.js
+++ src/strings/convert.js
@@ -36,7 +36,35 @@ export default {
   toUcfirst: function(value) {
     return value.substr(0, 1).toUpperCase() + value.substr(1, value.length);
   },
+  /**
+   * 首字母小写
+   *
+   * @param {string} value 转换字符串
+   *
+   * @return {string}
+   */
   toLcfirst: function(value) {
     return value.substr(0, 1).toLowerCase() + value.substr(1, value.length);
+  },
+  /**
+   * 格式化货币
+   * 
+   * @param {string|number} value 
+   * @param {string} symbol 货币符号 default($)
+   * @param {number} decimalDigits 小数位数
+   * @param {string} symbolPosition 位置(first|last)
+   *
+   * @return {string}
+   */
+  currency: function (value, symbol = "$", decimalDigits = 0, symbolPosition = 'first') {
+    var format = Number(value).toLocaleString('en-US');
+    if (decimalDigits != 0) {
+      format = format.toString() + '.';
+      for (let i = 0; i < decimalDigits; i++) {
+        format += '0';
+      }
+    }
+
+    return symbolPosition == 'first' ? symbol + format : format + ' ' + symbol;
   }
 };
